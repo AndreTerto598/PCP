a826c6803c0a2a8f72fa4ff8a5893f35a896f03a
André <tertocomercial@gmail.com>
2024-10-08 15:04:54 -0300

    Integrando a página de Cadastro com Flask para receber os dados que o usuário digita

--- PCP/main.py
+++ PCP/main.py
@@ -1,1 +1,29 @@
-import flask
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'TERTOPCP'
+
+
+@app.route('/cadastro', methods=['GET'])
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/Cadastro', methods=['POST'])
+def processar_cadastro():
+    usuario = request.form['usuario']
+    email = request.form['email']
+    senha = request.form['senha']
+    confirmar_senha = request.form['confirmar_senha']
+    print(usuario)
+    print(senha)
+    if senha != confirmar_senha:
+        return "As senhas não conferem! Tente novamente."
+    return f"Usuário {usuario} cadastrado com sucesso!"
+    
+    
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
