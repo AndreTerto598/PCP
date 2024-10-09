5577171863570d3b047b0b84d181515a174336da
AndreTerto598 <tertocomercial@gmail.com>
2024-10-08 23:20:15 -0300

    Realizada a integração entre Flask e DB, Página de Cadastros totalmente funcional.

--- PCP/main.py
+++ PCP/main.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'TERTOPCP'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg://postgres:123@localhost/PCP'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 
-@app.route('/cadastro', methods=['GET'])
+#Integração do DB+ Flask pra salvar os dados do cadastro
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    senha = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Usuario {self.usuario}>'
+
+@app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
     return render_template('cadastro.html')
 
@@ -14,11 +28,18 @@ def processar_cadastro():
     email = request.form['email']
     senha = request.form['senha']
     confirmar_senha = request.form['confirmar_senha']
-    print(usuario)
-    print(senha)
+    
     if senha != confirmar_senha:
         return "As senhas não conferem! Tente novamente."
+    
+    novo_usuario = Usuario(usuario=usuario, email=email, senha=senha)
+
+    db.session.add(novo_usuario)
+    db.session.commit()
+
     return f"Usuário {usuario} cadastrado com sucesso!"
+
+#Fim da parte de Cadastro
     
     
 
